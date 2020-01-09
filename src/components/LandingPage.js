ff32c4bd33cd367c516685a46d81cd8694f9a0c5
Prithviraj Khandekar <khandekarp@outlook.com>
2020-01-09 13:24:24 -0500

    tweak header

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -29,7 +29,7 @@ const LandingPage = (props) => {
                 logoC={logos.logoWhite}
                 color="transparent"
                 changeColorOnScroll={{
-                    height: 600,
+                    height: 550,
                     color: "white"
                 }}
             />
