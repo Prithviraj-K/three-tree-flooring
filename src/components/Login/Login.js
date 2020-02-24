f9c23c94e6b33a86b6813f9cf1963c66e54a5e2a
Prithviraj Khandekar <khandekarp@outlook.com>
2020-02-24 13:28:34 -0500

    done: login page design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,32 +4,84 @@ import HeaderLinks from '../Header/HeaderLinks';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import { CardActionArea } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import GoogleIcon from '../../assets/img/google-login.png';
+import Jump from 'react-reveal/Jump';
 
-const logoBlack = require('../../assets/img/logo-black-large.png');
+import fbLogin from '../../assets/img/fb-login.png';
+
+//styles
+import { makeStyles } from '@material-ui/core';
+import styles from './styleLogin';
+const useStyles = makeStyles(styles);
+
+
+const logoBlack = require('../../assets/img/logo-white.png');
 
 const Login = () => {
+
+    const classes = useStyles();
     return (
         <div>
             <Header
                 rightLinks={<HeaderLinks />}
                 fixed
                 logoC={logoBlack}
-                color="white"
+                color="transparent"
             />
-            <Grid 
+            <Grid
                 container
                 spacing={0}
                 direction="column"
                 alignItems="center"
                 justify="center"
-                style={{minHeight: "100vh"}}
+                className={classes.containerGrid}
             >
-                <Grid item xs={8}>
-                    <Card>
-                        <CardContent>
-                            Login
-                        </CardContent>
-                    </Card>
+                <Grid item xs={12} sm={12} md={12} lg={12}>
+                    <Jump>
+                        <Card className={classes.loginCard}>
+                            <CardContent className={classes.textCard}>
+                                <Typography variant="h5">
+                                    Welcome Back
+                                </Typography>
+                            </CardContent>
+                            <Divider className={classes.dividerText} />
+                            <FormControl className={classes.formCard}>
+                                <TextField
+                                    className={classes.textfieldCard}
+                                    id="outlined-basic"
+                                    label="Email"
+                                    variant="outlined"
+                                />
+                                <TextField
+                                    className={classes.textfieldCard}
+                                    id="outlined-basic"
+                                    label="Password"
+                                    variant="outlined"
+                                />
+                                <Button className={classes.btnCard}>
+                                    Login
+                                </Button>
+                            </FormControl>
+                            <div className={classes.divBtn}>
+                                <Button color="primary" className={classes.loginBtn}>
+                                    <FacebookIcon style={{paddingRight: "0.5em"}}/>
+                                    Continue with Facebook
+                                </Button>
+                            </div>
+                            <div className={classes.divBtn}>
+                                <Button className={classes.loginBtn}>
+                                    <img className={classes.googleBtn} src={GoogleIcon} alt="Google"/> Sign in with Google
+                                </Button>
+                            </div>
+                        </Card>
+                    </Jump>
                 </Grid>
             </Grid>
         </div>
---
+++ src/components/Login/styleLogin.js
@@ -0,0 +1,75 @@
+const styleLogin = {
+    containerGrid: {
+        minHeight: "100vh",
+        backgroundColor: "#99968e"
+    },
+    loginCard: {
+        boxShadow: "0 8px 40px -12px rgba(0,0,0,0.8) !important",
+        borderRadius: "2em",
+        width: "30vw",
+        padding: "2em"
+    },
+    textCard: {
+        textAlign: "center",
+        marginTop: "1em"
+    },
+    dividerText: {
+        width: "40%",
+        margin: "0em auto 1em auto"
+    },
+    formCard: {
+        width: "100%",
+    },
+    textfieldCard: {
+        width: "90%",
+        margin: "1em auto"
+    },
+    btnCard: {
+        width: "40%",
+        height: "3em",
+        margin: "1em auto 1em auto",
+        border: "1px solid black"
+    },
+    loginBtn: {
+        padding:"1em 2em"
+    },
+    divBtn: {
+        width: "100%",
+        height: "3em",
+        alignContent: "center",
+        margin: "auto",
+        justify: "center",
+        textAlign: "center"
+    },
+    googleBtn: {
+        height: "1.5em",
+        paddingRight: "1em"
+    },
+    '@media (max-width: 1280px)': {
+        loginCard: {
+            width: "50vw",
+        },
+        btnCard: {
+            width: "50%"
+        }
+    },
+    '@media (max-width: 960px)': {
+        loginCard: {
+            width: "60vw",
+        },
+        btnCard: {
+            width: "60%"
+        }
+    },
+    '@media (max-width: 600px)': {
+        loginCard: {
+            width: "80vw",
+            padding: "2em"
+        },
+        btnCard: {
+            width: "70%"
+        }
+    }
+}
+
+export default styleLogin;
